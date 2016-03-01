89f2a7f6d56852ec3f016646746754b84d6bb326
NarendraThadani <narendra.thadani@gmail.com>
2016-03-02 02:25:48 +0530

    included code to track invalid records with Counters

--- mapreduce-demo2/src/main/java/org/irobin/hadoop/demo2/AggregateMinDecade.java
+++ mapreduce-demo2/src/main/java/org/irobin/hadoop/demo2/AggregateMinDecade.java
@@ -9,68 +9,77 @@ import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 
 public class AggregateMinDecade {
-	
+
 	public static class AggregateMapper extends Mapper<Object, Text, Text, Text> {
-	
-	
-		@Override		
+
+		enum Aggregation {  NONNUMERIC_YEAR};
+		
+		@Override
 		protected void map(Object key, Text value, Mapper<Object, Text, Text, Text>.Context context)
 				throws IOException, InterruptedException {
 
 			StringTokenizer tokenizer = new StringTokenizer(value.toString());
-			
+
 			String year = tokenizer.nextToken();
 			tokenizer.nextToken();
 			tokenizer.nextToken();
 			String delta = tokenizer.nextToken();
+
+			try {
+				int tempYear;
+				tempYear = Integer.parseInt(year);
+				context.write(new Text("decade" + "_" + (tempYear - (tempYear % 10))), new Text(year + "_" + delta));
+			} catch (NumberFormatException nfe) {
+				System.err.println("Error: Non Numeric Value for Year:" + year);
+				context.setStatus("Detected Possibly incorrect Record: See Logs.");
+				context.getCounter(Aggregation.NONNUMERIC_YEAR).increment(1);
+			}
 			
-			int tempYear;
-			tempYear = Integer.parseInt(year);
-			context.write(new Text("decade" + "_" + (tempYear- (tempYear%10)) ), new Text( year + "_" + delta));
-			
-			
+
 		}
-		
+
 	}
+
 	public static class AggregateReducer extends Reducer<Text, Text, Text, FloatWritable> {
-		
+
 		@Override
 		protected void reduce(Text key, Iterable<Text> values, Reducer<Text, Text, Text, FloatWritable>.Context context)
 				throws IOException, InterruptedException {
-		
 
 			String compositeString;
-			String []compositeStringArray;
+			String[] compositeStringArray;
 			Text tempYear;
 			long tempValue;
 			long min;
 			Text minYear;
-			
+
 			minYear = new Text("NULL");
 			min = Long.MAX_VALUE;
 			for (Text text : values) {
 				compositeString = text.toString();
 				compositeStringArray = compositeString.split("_");
-				
-				tempYear = new Text (compositeStringArray[0]);
-				tempValue = new Long(compositeStringArray[1]   ).longValue();
-				
-				if(tempValue < min){
-					
-					min = tempValue ;
-					minYear = tempYear;
+
+				tempYear = new Text(compositeStringArray[0]);
+				try {
+					tempValue = new Long(compositeStringArray[1]).longValue();
+
+					if (tempValue < min) {
+
+						min = tempValue;
+						minYear = tempYear;
+					}
+				} catch (NumberFormatException nfe) {
+					System.err.println("Error: Invalid Number " + compositeStringArray[0] + " : " + compositeStringArray[1]);
+
 				}
-				
-				
+
 			}
-			
-			Text keyText = new Text ("min(" + key.toString() + "_" + minYear.toString() + "): ");
+
+			Text keyText = new Text("min(" + key.toString() + "_" + minYear.toString() + "): ");
 			context.write(keyText, new FloatWritable(min));
-			
+
 		}
-		
-		
+
 	}
-	
 
 }
